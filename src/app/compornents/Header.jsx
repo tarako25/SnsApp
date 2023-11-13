17b6699fbe3ec7f22384b7b2a613e6abefe0e312
tarako25 <nishimura1603t@gmail.com>
2023-11-13 23:43:39 +0900

    メニューレスポンシブ修正

--- src/app/compornents/Header.jsx
+++ src/app/compornents/Header.jsx
@@ -7,11 +7,11 @@ export default function Header() {
     <>
       <div className='mt-4 mb-5 w-full md:mt-7 md:mb-10 '>
         <div className='flex justify-center items-center w-full'>
-          <div className='w-full flex justify-between'>
+          <div className='w-full flex justify-between items-center'>
             <Link href="/">
               <Image src={Logo} className='w-[125px] h-[45px] md:w-[175px] md:h-[60px]' alt="Logo"/>
             </Link>
-            <div className='flex justify-center items-center px-2 rounded-md border-4 border-neutral-400 normal hover:bg-neutral-400 cursor-pointer'>
+            <div className='h-[40px] md:[60px] flex justify-center items-center px-2 rounded-md border-4 border-neutral-400 normal hover:bg-neutral-400 cursor-pointer'>
               <div className='text-centerfont-bold text-xs tracking-[1px] text-white md:text-base'>アカウント切り替え</div>
             </div>
           </div>
--- src/app/compornents/MenuBar.jsx
+++ src/app/compornents/MenuBar.jsx
@@ -6,17 +6,19 @@ import { MenuElements } from "@/lib/Elements"
 export default function MenuBar() {
   return (
     <>
-      <div className=' rounded w-[100%] flex justify-start flex-row md:w-[25%] md:flex-col'>
-        <div className='w-[100%] flex-row flex justify-between md:flex-col pb-2 md:pb-0'>
-          {MenuElements.map((items, index) =>
-            <Link href={items.link} key={index} className={`cursor-pointer flex justify-center items-center h-[45px] rounded-md border-4 md:h-[65px] md:mb-3 w-[20%] md:w-[100%]  border-neutral-400 normal hover:bg-neutral-400 md:flex ${items.class === 'none' ? 'hidden' : ''}`}>
-              <div className='text-white mx-3'>{items.icon}</div>
-              <div className='w-full text-left font-bold text-sm tracking-[3px] hidden text-white md:block xl:text-lg'>{items.MenuName}</div>
-            </Link>
-          )}
+      <div className='w-[100%] md:w-[30%] flex'>
+        <div className='rounded w-[100%] flex justify-start flex-row md:flex-col'>
+          <div className='w-[100%] flex-row flex justify-between md:flex-col pb-2 md:pb-0'>
+            {MenuElements.map((items, index) =>
+              <Link href={items.link} key={index} className={`cursor-pointer flex justify-center items-center h-[45px] rounded-md border-4 md:h-[65px] md:mb-3 w-[20%] md:w-[100%]  border-neutral-400 normal hover:bg-neutral-400 md:flex ${items.class === 'none' ? 'hidden' : ''}`}>
+                <div className='text-white mx-3'>{items.icon}</div>
+                <div className='w-full text-left font-bold text-sm tracking-[3px] hidden text-white md:block xl:text-lg'>{items.MenuName}</div>
+              </Link>
+            )}
+          </div>
         </div>
+        <div className='border-2 borderline mx-4 hidden md:block'></div>
       </div>
-      <div className='border borderline'></div>
     </>
   )
 }
