35bd22dbc35517ac6cace0d52445200b0e91ec0d
nishimura <nishimura1603t@gmail.com>
2023-11-14 13:20:24 +0900

    スマホレスポンシブ画面拡大

--- src/app/compornents/Main.jsx
+++ src/app/compornents/Main.jsx
@@ -4,7 +4,11 @@ import React from 'react'
 export default function Main() {
   return (
     <>
-    <div className='normal border-4 w-[100%] rounded-md border-neutral-400 md:w-[70%]'>Main Commit</div>
+    <div className='normal border-4 w-[100%] rounded-md border-neutral-400 md:w-[70%]'>
+      <div className='bg-white w-fullrounded-md'>
+        Main Commit
+      </div>
+    </div>
     </>
   )
 }
