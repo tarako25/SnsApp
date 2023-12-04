73372fb24e4f7fca942709a5bb4fcddb3e4c9dd1
tarako25 <nishimura1603t@gmail.com>
2023-12-04 23:53:24 +0900

    プロフx－る画面作成中

---
+++ src/app/components/Profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Profile(data: any) {
+  return (
+    <>
+    <div className='normal border-4 w-[100%] rounded-md border-neutral-400 md:w-[70%]'>
+      <div className='w-full pb-3 rounded-sm normal flex justify-center'>
+        <div className='w-[95%]'>
+
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
