ddb175b8df401b6d07c7509aca0d30c1f730e0e1
tarako25 <nishimura1603t@gmail.com>
2023-12-18 21:10:33 +0900

    コミット

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,35 +1,6 @@
 import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import { PrismaClient } from "@prisma/client"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { authOptions } from '@/lib/AuthOption'
+ 
+const handler = NextAuth(authOptions)
 
-const prisma = new PrismaClient()
-
-const authOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID ?? '',
-        clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
-      }),
-      GoogleProvider({
-          clientId: process.env.Google_CLIENT_ID ?? '',
-          clientSecret: process.env.Google_CLIENT_SECRET ?? '',
-        }),
-    ],
-    pages: {
-      signIn: '/auth/signin',
-    },
-    callbacks: {
-        session: async ({ session, user }: { session: any; user: any }) => {
-          if (user) {
-            session.user.id = user.id; // userIdをセッションに追加
-          }
-          return session;
-        },
-      },
-}
-
-export default NextAuth(authOptions)
+export { handler as GET, handler as POST }
--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -10,8 +10,8 @@ export default function page() {
     <>
     <div className='flex justify-center items-center h-screen'>
       <div className='w-full sm:w-[450px] bg-white'>
-        <div className='pt-[0px] sm:py-[50px] flex justify-start items-center flex-col bg-white sm:bg-neutral-500 w-full h-full rounded-xl'>
-          <Image src={Logo} alt="" className='w-[175px] h-[60px] mb-7 sm:w-[215px] sm:h-[75px]'/>
+        <div className='pt-[0px] sm:py-[50px] flex justify-start items-center flex-col bg-white w-full h-full rounded-xl'>
+          <Image src={Logo} alt="" className='w-[175px] h-[60px] sm:w-[215px] sm:h-[75px]'/>
           {/* <div className='flex justify-center items-start flex-col'>
             <label htmlFor="" className='font-bold text-black mb-1 text-sm'>メールアドレス</label>
             <input type="text" placeholder="email" className='pl-2 rounded mb-3 w-[350px] h-[40px] border-2'/>
@@ -21,6 +21,7 @@ export default function page() {
               ログイン
             </button>
           </div> */}
+
           <div className='w-[350px] border my-4 border-zinc-400'></div>
           <button onClick={() => signIn("google")} className='bg-white mb-2 font-bold rounded w-[350px] h-[40px] border-2'>
             Googleでログイン
