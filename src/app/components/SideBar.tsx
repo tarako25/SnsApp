a8509f61b554a18430d39736f801945ea8455503
tarako25 <nishimura1603t@gmail.com>
2023-12-25 22:14:48 +0900

    ログアウトおまけ

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { useSession, signOut, signIn } from "next-auth/react"
 
 export default function SideBar(data: any) {
   return (
     <>
     <div className='hidden bg-white border border-color rounded xl:w-[30%] xl:block'>
         <div className='flex items-center flex-col'>
-            aaa
+        <button onClick={() => signOut()} className='bg-white mb-2 font-bold rounded w-[350px] h-[40px] border-2'></button>
         </div>
     </div>
     </>
