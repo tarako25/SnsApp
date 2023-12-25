5bcbc70d7dcbba3b50408b2962feb612eb19beca
tarako25 <nishimura1603t@gmail.com>
2023-12-25 21:15:22 +0900

    テスト

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,13 +14,6 @@ function Loginpage() {
     <div className="relative flex justify-center items-center w-full">
       <div className="w-[100%] flex justify-center items-center flex-col md:w-[80%] xl:w-[1025px]">
         <SignIn />
-        <div>NEXTAUTH_URL: {process.env.NEXTAUTH_URL}</div>
-        <div>NEXTAUTH_SECRET: {process.env.NEXTAUTH_SECRET}</div>
-        <div>GITHUB_CLIENT_ID: {process.env.GITHUB_CLIENT_ID}</div>
-        <div>GITHUB_CLIENT_SECRET: {process.env.GITHUB_CLIENT_SECRET}</div>
-        <div>GOOGLE_CLIENT_ID: {process.env.GOOGLE_CLIENT_ID}</div>
-        <div>GOOGLE_CLIENT_SECRET: {process.env.GOOGLE_CLIENT_SECRET}</div>
-        <div>DATABASE_URL: {process.env.DATABASE_URL}</div>
       </div>
     </div>
     </>
