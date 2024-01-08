d0b41a5cf93a587f37fb0c42fcf4972cc34a5f19
tarako25 <nishimura1603t@gmail.com>
2024-01-08 23:43:01 +0900

    検索ボックス設置

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { useSession, signOut, signIn } from "next-auth/react"
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function SideBar(data: any) {
   return (
     <>
-    <div className='hidden bg-white border border-color rounded xl:w-[30%] xl:block'>
-        <div className='flex items-center flex-col'>
-        <button onClick={() => signOut()} className='bg-white my-2 font-bold rounded w-[350px] h-[40px] border-2'>ログアウト(仮)</button>
-        </div>
+    <div className='hidden bg-white border border-color  items-center flex-col rounded xl:w-[30%] xl:flex'>
+        <div className='flex items-center flex-col w-11/12'>
+          <div className="relative mt-3 flex justify-start items-center w-full">
+            <SearchIcon className="absolute mx-3 text-gray-400" />
+            <input type="text" className='border-color w-full rounded-2xl h-[35px] px-10' placeholder='ユーザーを検索'/>
+          </div>
+          <button onClick={() => signOut()} className='bg-white mt-3 font-bold w-full h-[40px] border-2'>ログアウト(仮)</button>
+          <p>ユーザー検索</p>
+          <p>トレンド</p>
+          <p>天気予報(設定で県検索している場合)</p>
+          </div>
     </div>
     </>
   )
