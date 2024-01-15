36439489b5eadd0208cca1211bbc1fefa8a836c8
tarako25 <nishimura1603t@gmail.com>
2024-01-15 23:28:24 +0900

    タグ１００％の時2段にしたい。あとundefinedのせいで#なしでも要素出来る

--- src/app/components/InputPost.tsx
+++ src/app/components/InputPost.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import sample from '@/imgs/sample2.png'
 import toast, { Toaster } from "react-hot-toast";
 import { z } from 'zod';
-import ph  from "@/app/components/ParseHashtags"
+import pt  from "@/app/components/ParseTag"
 
 const postDataSchema = z.object({
   content: z.string().min(1, "投稿内容を入力してください").max(150, "内容は150文字以内で入力してください"),
@@ -16,12 +16,11 @@ const postDataSchema = z.object({
 export default function InputPost(data: any) {
 
   const [error, setError] = useState<string | null>(null);
-  const [content, setContent] = useState<string | null>(null);
+  const [content, setContent] = useState<string[] | null>([]);
 
   const handleChangeText = (e: any) => {
-    const text = ph((e.target as HTMLElement).textContent);
+    const text = pt((e.target as HTMLElement).textContent);
     setContent(text);
-    console.log("test",e.target.textContent)
   }
 
   const handleInput = async(e: any) => {
@@ -63,11 +62,15 @@ export default function InputPost(data: any) {
       <div className=' rounded mt-3 bg-white flex justify-start items-center flex-col border-color'>
         <div className='flex w-[95%] mt-3'>
           <Image alt="" src={data.img || sample} width={55} height={55} className='w-[55px]  h-[55px] rounded-full border-color'/>
-          <form onSubmit={handleInput} className='relative flex justify-center w-full items-center my-1 flex-col text-left'>
-            <div style={{color: 'transparent'}} contentEditable onInput={handleChangeText} className='absolute z-10 placeholder w-[95%] h-[45px] p-2 text-sm md:text-base rounded-md border-color flex items-start justify-start'>
-            </div>
-            <div contentEditable onInput={handleChangeText} className='absolute z-1 placeholder w-[95%] h-[45px] p-2 text-sm md:text-base rounded-md border-color flex items-start justify-start'>
-              {content} 
+          <form onSubmit={handleInput} className='flex justify-center w-full items-center flex-col '>
+            <div className='flex justify-center w-full items-center flex-col text-left mt-1'>
+              <div contentEditable onInput={handleChangeText} className='placeholder w-[95%] min-h-[45px] p-2 text-sm md:text-base rounded-md border-color flex items-start justify-start'>
+              </div>
+              <div className='flex justify-start items-center w-[95%] mt-3'>
+              {content && content.map((item: any) => (
+                <div className='border-color bg-color text-color px-3 py-1 mr-3 rounded-md'>{item}</div>
+              ))}
+              </div>
             </div>
             <div className='w-[95%] felx justify-center items-center my-2'>
               <div className='flex justify-start'>
